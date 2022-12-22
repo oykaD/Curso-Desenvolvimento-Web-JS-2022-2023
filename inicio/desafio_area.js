bde26d261e21b593a13629d6986119cc325435ed
Gabriel Silva <90668697+Oykad@users.noreply.github.com>
2022-12-22 19:48:36 -0300

    Add files via upload

---
+++ inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + "m².");
+
+areaCirc = Math.PI * raio * raio
+console.log("O valor da área é " + areaCirc + "m².");
---
+++ inicio/sentenca_1.js
@@ -0,0 +1,3 @@
+console.log("Yes");
+console.log("No");
+console.log("Maybe");

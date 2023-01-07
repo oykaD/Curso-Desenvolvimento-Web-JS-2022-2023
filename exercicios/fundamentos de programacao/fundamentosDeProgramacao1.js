3586f1c77f08b56c2a3fac21b92859b83908c5c1
Gabriel Silva <90668697+oykaD@users.noreply.github.com>
2023-01-07 15:07:31 -0300

    Add files via upload

---
+++ exercicios/fundamentos de programacao/fundamentosDeProgramacao1.js
@@ -0,0 +1,10 @@
+// Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a 
+// função) e com ponto de exclamação "!" no final.
+
+function dizerOla(text) {
+    return "Olá, " + text + "!"
+}
+
+console.log(dizerOla("Fulano"))
+console.log(dizerOla("Ciclano"))
+console.log(dizerOla("Beltrano"))
---
+++ exercicios/fundamentos de programacao/fundamentosDeProgramacao2.js
@@ -0,0 +1,10 @@
+// Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade 
+// em dias
+
+function idadeEmDias(idadeAnos) {
+    return idadeAnos * 365
+}
+
+console.log(idadeEmDias(15))
+console.log(idadeEmDias(24))
+console.log(idadeEmDias(30))
